655a48f531e05808839c57f23d842fb685f72057
mahek <mahektailor28@gmail.com>
2023-10-15 11:26:57 -0400

    Added Test For Split

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Solution;
+
+public class SplitCalculator
+{
+    public decimal SplitAmount(decimal totalAmount, int numberOfPeople)
+    {
+        if (numberOfPeople <= 0)
+            throw new ArgumentException("Number of people must be greater than zero.");
+
+        if (totalAmount < 0)
+            throw new ArgumentException("Total amount cannot be negative.");
+
+        return totalAmount / numberOfPeople;
+    }
+}
--- SolutionTest/UnitTest1.cs
+++ SolutionTest/UnitTest1.cs
@@ -1,11 +1,47 @@
-namespace SolutionTest
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution;
+
+
+
+[TestClass]
+public class SplitCalculatorTests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void SplitAmount_EqualAmountAndPeople_ReturnsOriginalAmount()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        // Arrange
+        SplitCalculator calculator = new SplitCalculator();
+        decimal totalAmount = 100.00M;
+        int numberOfPeople = 10;
+
+        // Act
+        decimal result = calculator.SplitAmount(totalAmount, numberOfPeople);
+
+        // Assert
+        Assert.AreEqual(10.00M, result);
+    }
+
+    [TestMethod]
+    public void SplitAmount_ZeroAmount_ThrowsException()
+    {
+        // Arrange
+        SplitCalculator calculator = new SplitCalculator();
+        decimal totalAmount = 0;
+        int numberOfPeople = 5;
+
+        // Act and Assert
+        Assert.ThrowsException<ArgumentException>(() => calculator.SplitAmount(totalAmount, numberOfPeople));
+    }
+
+    [TestMethod]
+    public void SplitAmount_NegativePeople_ThrowsException()
+    {
+        // Arrange
+        SplitCalculator calculator = new SplitCalculator();
+        decimal totalAmount = 50.00M;
+        int numberOfPeople = -2;
+
+        // Act and Assert
+        Assert.ThrowsException<ArgumentException>(() => calculator.SplitAmount(totalAmount, numberOfPeople));
     }
 }
