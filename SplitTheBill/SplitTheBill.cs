71c62fc3cd0fcce06b8c9da598afda3ca39ed5f9
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-02 12:40:48 -0500

    Added Tip Caculator

--- SplitTheBill/SplitTheBill.cs
+++ SplitTheBill/SplitTheBill.cs
@@ -14,4 +14,37 @@ namespace SplitTheBill
             return decimal.Round(amount / numberOfPeople, 2);
         }
     }
+
+    public class TipCalculator
+    {
+        public static Dictionary<string, decimal> CalculateTipShares(Dictionary<string, decimal>? mealCosts, float tipPercentage)
+        {
+            if (mealCosts == null)
+            {
+                throw new ArgumentNullException(nameof(mealCosts), "Meal costs dictionary cannot be null.");
+            }
+
+            if (tipPercentage < 0 || tipPercentage > 100)
+            {
+                throw new ArgumentException("Tip percentage must be between 0 and 100.", nameof(tipPercentage));
+            }
+
+            Dictionary<string, decimal> tipShares = new Dictionary<string, decimal>();
+
+            decimal totalMealCost = 0;
+
+            foreach (var kvp in mealCosts!)
+            {
+                totalMealCost += kvp.Value;
+            }
+
+            foreach (var kvp in mealCosts!)
+            {
+                decimal individualTipShare = kvp.Value / totalMealCost * (decimal)tipPercentage / 100;
+                tipShares.Add(kvp.Key, individualTipShare);
+            }
+
+            return tipShares;
+        }
+    }
 }
