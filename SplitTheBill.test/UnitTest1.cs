6bf8b5cc7be4c1729b2ed2c0204f76910413ae99
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-02 12:43:12 -0500

    Added Test for Tip Calculator

--- SplitTheBill.test/UnitTest1.cs
+++ SplitTheBill.test/UnitTest1.cs
@@ -42,4 +42,62 @@ namespace splitthebill.test
             Assert.ThrowsException<ArgumentException>(() => BillSplitter.SplitAmount(amount, numberOfPeople));
         }
     }
+
+    [TestClass]
+    public class TipCalculatorTests
+    {
+        [TestMethod]
+        public void TestCalculateTipShares()
+        {
+            // Arrange
+            Dictionary<string, decimal> mealCosts = new Dictionary<string, decimal>
+            {
+                {"Mahek", 20.00m},
+                {"Akshar", 30.00m},
+                {"Krupa", 25.00m}
+            };
+
+            float tipPercentage = 15;
+
+            // Act
+            Dictionary<string, decimal> result = TipCalculator.CalculateTipShares(mealCosts, tipPercentage);
+
+            // Assert
+            Assert.AreEqual(3.00m, result["Mahek"]);
+            Assert.AreEqual(4.50m, result["Akshar"]);
+            Assert.AreEqual(3.75m, result["Krupa"]);
+        }
+
+        [TestMethod]
+        public void TestCalculateTipSharesWithNullMealCosts()
+        {
+            // Arrange
+            Dictionary<string, decimal> mealCosts = null;
+            float tipPercentage = 15;
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() => TipCalculator.CalculateTipShares(mealCosts, tipPercentage));
+        }
+
+        [TestMethod]
+        public void TestCalculateTipSharesWithInvalidTipPercentage()
+        {
+            // Arrange
+            Dictionary<string, decimal> mealCosts = new Dictionary<string, decimal>
+            {
+                {"Mahek", 20.00m},
+                {"Akshar", 30.00m},
+                {"Krupa", 25.00m}
+            };
+            float tipPercentage = -5;
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => TipCalculator.CalculateTipShares(mealCosts, tipPercentage));
+        }
+    }
+
+
+
+
+
 }
